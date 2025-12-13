1cff46e29dda56362ad9eebb1c1c059149eb6e46
William González <ruyk1120@gmail.com>
2025-12-13 11:06:42 -0500

    Initial Commit

---
+++ YunoTalkTrack/src/main/java/co/yuno/YunoTalkTrack/YunoTalkTrackApplication.java
@@ -0,0 +1,13 @@
+package co.yuno.YunoTalkTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunoTalkTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YunoTalkTrackApplication.class, args);
+	}
+
+}
---
+++ YunoTalkTrack/src/test/java/co/yuno/YunoTalkTrack/YunoTalkTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package co.yuno.YunoTalkTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YunoTalkTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
